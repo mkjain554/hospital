0fc521f74b6f395b9a288a0222c2fe01fbd3a0d0
Manjeet Sangwan <manjeet.sangwan@mproga1.com>
2016-02-23 03:20:17 +0530

    ok

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,9 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+var fb = new Firebase("https://intense-inferno-9747.firebaseio.com/");
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova', 'firebase'])
+
 
 .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
@@ -42,6 +44,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
             url: '/register',
             templateUrl: 'templates/registration.html',
             controller: 'RegisterCtrl'
+        })
+        .state('patientlist', {
+            url: '/patientlist',
+            templateUrl: 'templates/patientlist.html',
+            controller: 'PatientListCtrl'
         })
         // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/login');
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,25 +4,89 @@ angular.module('starter.controllers', [])
     $scope.logout = function () {
         $state.go("login");
     }
-    $scope.opdPatient = function () {
-        $state.go("register");
-    }
-    $scope.admitPatient = function () {
-        $state.go("register");
+    $scope.listPatient = function () {
+        $state.go("patientlist");
     }
 })
 
-.controller('RegisterCtrl', function ($scope, $state, $ionicPopup) {
+.controller('RegisterCtrl', function ($scope, $state, $ionicPopup, $firebaseArray, $cordovaCamera) {
+    $scope.data = {
+        firstname: '',
+        lastname: '',
+        age: '',
+        mobilenumber: '',
+        address: '',
+        email: '',
+        imageurl: "../images/default.png"
+    };
+
     $scope.takePicture = function () {
-        navigator.camera.getPicture(function (imageURI) {
-            // imageURI is the URL of the image that we can use for
-            // an <img> element or backgroundImage.
-        }, function (err) {
-// Ruh-roh, something bad happened
-        }, cameraOptions);
+        var options = {
+            quality: 75,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            allowEdit: true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 150,
+            targetHeight: 150,
+            saveToPhotoAlbum: false
+        };
+        $cordovaCamera.getPicture(options).then(function (imageData) {
+            console.log(imageData);
+            $scope.data.imageurl = "data:image/gif;base64," + imageData;
+        }, function (error) {
+            console.error(error);
+        });
+    }
+    $scope.addPatient = function () {
+        $scope.data.id = new Date().getUTCMilliseconds();
+        if (localStorage.getItem("patients")) {
+            var patients = JSON.parse(localStorage.getItem("patients"));
+            patients.push($scope.data);
+            localStorage.setItem("patients", JSON.stringify(patients));
+        } else {
+            localStorage.setItem("patients", JSON.stringify([$scope.data]));
+        }
     }
 })
 
+
+.controller('PatientListCtrl', function ($scope, LoginService, $ionicPopup, $state) {
+    $scope.showReorder = false;
+    $scope.showDelete = false;
+    $scope.patients = [];
+    if (localStorage.getItem("patients")) {
+        $scope.patients = JSON.parse(localStorage.getItem("patients"));
+    }
+    console.log("$scope.patients..." + JSON.stringify($scope.patients));
+    $scope.addPatient = function () {
+        $state.go("register");
+    }
+    $scope.moveItem = function (item, fromIndex, toIndex) {
+        $scope.patients.splice(fromIndex, 1);
+        $scope.patients.splice(toIndex, 0, item);
+    }
+    $scope.onItemDelete = function (item) {
+        var index;
+        for (var i = 0; i < $scope.patients.length; i++) {
+            if ($scope.patients[i].id == item.id) {
+                index = i;
+            }
+        }
+        $scope.patients.splice(index, 1);
+    }
+    $scope.doRefresh = function () {
+        if (localStorage.getItem("patients")) {
+            $scope.patients = JSON.parse(localStorage.getItem("patients"));
+            $scope.$broadcast('scroll.refreshComplete');
+        } else {
+            $scope.$broadcast('scroll.refreshComplete');
+        }
+    }
+    $scope.toggleStar = function (item) {
+        item.star = !item.star;
+    }
+})
 
 .controller('LoginCtrl', function ($scope, LoginService, $ionicPopup, $state) {
     $scope.data = {
@@ -30,6 +94,8 @@ angular.module('starter.controllers', [])
         password: ''
     };
     $scope.login = function () {
+        $scope.data.username = "user";
+        $scope.data.password = "1234";
         LoginService.loginUser($scope.data.username, $scope.data.password).success(function (data) {
             $state.go('dashboard');
         }).error(function (data) {
